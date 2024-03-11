cf9cb7ade08a30501c0577386256b6d3d4cc4746
kvon <kvon.e.a@gmail.com>
2024-03-11 13:34:34 +0300

    Version 1.1.0 PAN and Short Identifier Register is written.

--- dw1000.h
+++ dw1000.h
@@ -1,11 +1,45 @@
 #ifndef __DW1000__DW1000__H
 #define __DW1000__DW1000__H
 
-#define DW1000_SPI_BUF_SIZE 0x10
+/*	this buffer size is enough to write maximum (standard) number of bytes to TX_BUFFER	*/
+#define DW1000_SPI_BUF_SIZE 0x80
+#define DW1000_TFLEN 0x40
 
 #define DW1000_RST_High GPIOB->BSRR&=~GPIO_BSRR_BR1;\
 	GPIOB->BSRR|=GPIO_BSRR_BS1
 #define DW1000_RST_Low GPIOB->BSRR&=~GPIO_BSRR_BS1;\
 	GPIOB->BSRR|=GPIO_BSRR_BR1
+	
+/*	TX/RX call-back data (loaded from deca_device_api.h)	*/
+typedef struct
+{
+	uint32_t status;      //initial value of register as ISR is entered
+	uint16_t datalength;  //length of frame
+	uint8_t  fctrl[2];    //frame control bytes
+	uint8_t  rx_flags;    //RX frame flags, see above
+} dw_cb_data_t;
+
+/*	Call-back type for all events	(loaded from deca_device_api.h)	*/
+typedef void (*dw_cb_t)(const dw_cb_data_t *);
+
+/* 
+ * Structure to store device configuration.
+ * Loaded from deca_device_api.h
+ *
+ */
+typedef struct
+{
+	uint8_t chan ;           //!< channel number {1, 2, 3, 4, 5, 7 }
+	uint8_t prf ;            //!< Pulse Repetition Frequency {DWT_PRF_16M or DWT_PRF_64M}
+	uint8_t txPreambLength ; //!< DWT_PLEN_64..DWT_PLEN_4096
+	uint8_t rxPAC ;          //!< Acquisition Chunk Size (Relates to RX preamble length)
+	uint8_t txCode ;         //!< TX preamble code
+	uint8_t rxCode ;         //!< RX preamble code
+	uint8_t nsSFD ;          //!< Boolean should we use non-standard SFD for better performance
+	uint8_t dataRate ;       //!< Data Rate {DWT_BR_110K, DWT_BR_850K or DWT_BR_6M8}
+	uint8_t phrMode ;        //!< PHR mode {0x0 - standard DWT_PHRMODE_STD, 0x3 - extended frames DWT_PHRMODE_EXT}
+	uint16_t sfdTO ;         //!< SFD timeout value (in symbols)
+} dw_config_t ;
+	
 	
 #endif
--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 #include "dw1000.h"
 
 #include <memory.h>
-#include <stdio.h>
-
+#include <stdio.h> 
 
 char usart_tx_buf[CHAR_BUF_SIZE];
 char spi_tx_buf[DW1000_SPI_BUF_SIZE];
 char spi_rx_buf[DW1000_SPI_BUF_SIZE];
+
 
 /*	not in use	*/
 /*
@@ -17,15 +17,29 @@ void SysTick_Handler(){
 }
 */
 
+void goodbye_world(){
+	/*	turn on leds	*/
+	GPIOB->BSRR&=~GPIO_BSRR_BR8;
+	GPIOB->BSRR|=GPIO_BSRR_BS8;
+	GPIOB->BSRR&=~GPIO_BSRR_BR9;
+  GPIOB->BSRR|=GPIO_BSRR_BS9;
+  /*	*/
+	stm32_usart_tx("Goodbye, world!\n",0);
+}
+
 void main(void)
 {
 	int rv,i;
 	/*	register address	*/
 	uint16_t addr;
-	/*	use to write data to the only register	*/
+	/*	short buffer for read/write operations	*/
 	uint16_t value;
 	/*	SPI transaction size	*/
 	uint16_t spi_tr_size;
+	/*	sended message	*/
+	const char * msg="hello, world\n";
+	/*	buffer for read/write operations	*/
+	uint32_t value32;
 	
 	/*	PLLCLK settings	*/
 	stm32_pll_t stm32_pll;
@@ -89,15 +103,121 @@ void main(void)
 	/*	*/
 	spi_tr_size=0x8;
 	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
-	sprintf(usart_tx_buf,"dw1000 SPI buffer content: {0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x}",
+	sprintf(usart_tx_buf,"Device Identifier Register (reg:00:00):\n\t{0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x}\n",
 		spi_rx_buf[0],spi_rx_buf[1],spi_rx_buf[2],spi_rx_buf[3],spi_rx_buf[4],spi_rx_buf[5],spi_rx_buf[6],spi_rx_buf[7]);
 	stm32_usart_tx(usart_tx_buf,0);
-
-	/*	turn on leds	*/
-	GPIOB->BSRR&=~GPIO_BSRR_BR8;
-	GPIOB->BSRR|=GPIO_BSRR_BS8;
-	GPIOB->BSRR&=~GPIO_BSRR_BR9;
-  GPIOB->BSRR|=GPIO_BSRR_BS9;
-  /*	*/
-	stm32_usart_tx("Programm is finished\n",0);
+	
+	/*	set value to PAN Identifier and Short Address register (0x03) and check ii value	*/
+	spi_tx_buf[0]=0x80|0x03;
+	spi_tx_buf[1]=0x3;
+	spi_tx_buf[2]=0x2;
+	spi_tx_buf[3]=0x1;
+	spi_tx_buf[4]=0x0;
+	spi_tr_size=0x8;
+	/*	write 0x00010001 value to 0x03 register	*/
+	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
+	
+	/*	clear buffers and check written value	*/
+	memset(spi_tx_buf,0,DW1000_SPI_BUF_SIZE);
+	memset(spi_rx_buf,0,DW1000_SPI_BUF_SIZE);
+	spi_tx_buf[0]=0x3;
+	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
+	sprintf(usart_tx_buf,"PAN and Short Address Register (reg:03:00):\n\t{0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x}\n",
+		spi_rx_buf[0],spi_rx_buf[1],spi_rx_buf[2],spi_rx_buf[3],spi_rx_buf[4],spi_rx_buf[5],spi_rx_buf[6],spi_rx_buf[7]);
+	stm32_usart_tx(usart_tx_buf,0);
+	
+	goodbye_world();
+	return;
+	
+	/*	System Configuration Register (0x04)	*/
+	memset(spi_tx_buf,0,DW1000_SPI_BUF_SIZE);
+	memset(spi_rx_buf,0,DW1000_SPI_BUF_SIZE);
+	/* Allowed bits: 0:FFEN, 1:FFBC(?), 3:FFAD, 4:FFAA, 5:FFAM, 9:HIRQ_POL, 22:RXM110K, 30:AUTOACK
+	 * Summary : 0x40400272
+	 * Note: All of these fields are used in receiver mode.
+	 * To write register value LSB mode is used.
+	 */
+	spi_tx_buf[0]=0x80|0x04;
+	spi_tx_buf[1]=0x72;
+	spi_tx_buf[2]=0x02;
+	spi_tx_buf[3]=0x40;
+	spi_tx_buf[4]=0x40;
+	spi_tr_size=0x8;
+	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
+	/*	clear SPI buffers	*/
+	memset(spi_tx_buf,0,DW1000_SPI_BUF_SIZE);
+	memset(spi_rx_buf,0,DW1000_SPI_BUF_SIZE);
+	/*	check written value	*/
+	spi_tx_buf[0]=0x4;
+	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
+	sprintf(usart_tx_buf,"System Configuration Register (reg:04:00):\n\t{0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x}\n",
+		spi_rx_buf[0],spi_rx_buf[1],spi_rx_buf[2],spi_rx_buf[3],spi_rx_buf[4],spi_rx_buf[5],spi_rx_buf[6],spi_rx_buf[7]);
+	stm32_usart_tx(usart_tx_buf,0);
+	
+	/*	Transmit Frame Control Register (0x08)	*/
+	memset(spi_tx_buf,0,DW1000_SPI_BUF_SIZE);
+	memset(spi_rx_buf,0,DW1000_SPI_BUF_SIZE);
+	/*
+	 * Assigned values:
+	 *	TFLEN: 64; The length of data portion in bytes includin two-octet CRC appended at the end of the frame. Copied to the PHY header.
+	 *	TXBR: 0; User bit rate for the data portion of the frame (see receiver configurtation).
+	 *	TXPRF: 2'b10; 64MHz (see receiver configuration).
+	 *	TXPSR PE:4'b1100; 4096 preambule length in symbols (Recomended for 11pkBps bit rate).
+	 *	TXBOFFS: 0; Transmit buffer index offset.
+	 *	IFSDELAY: 0; Later (see receiver configuration)
+	 * Summary: reg:08:00 value is 0x00320020
+	 * To configure TX_FCTRL register sub-index (2-octet header SPI transaction) is used. 
+	*/
+	spi_tx_buf[0]=0xc8;
+	spi_tx_buf[1]=0x00;
+	spi_tx_buf[2]=0x20;
+	spi_tx_buf[3]=0x00;
+	spi_tx_buf[4]=0x32;
+	spi_tx_buf[5]=0x00;
+	spi_tr_size=0x8;
+	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
+	/* IFSDELAY field configuration (sub-index 0x04):
+	 * The value of this field will be defined later.
+	*/
+	memset(spi_tx_buf,0,DW1000_SPI_BUF_SIZE);
+	memset(spi_rx_buf,0,DW1000_SPI_BUF_SIZE);
+	spi_tx_buf[0]=0xc8;
+	spi_tx_buf[1]=0x04;
+	spi_tx_buf[2]=0x10;
+	stm32_spi_tr(spi_tx_buf, spi_rx_buf, spi_tr_size, 0);
+	/*	check written values (at sub-index 0x00)	*/
+	spi_tx_buf[0]=0x8;
+	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
+	sprintf(usart_tx_buf,"Transmit Frame Control Register (reg:08:00):\n\t{0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x}\n",
+		spi_rx_buf[0],spi_rx_buf[1],spi_rx_buf[2],spi_rx_buf[3],spi_rx_buf[4],spi_rx_buf[5],spi_rx_buf[6],spi_rx_buf[7]);
+	stm32_usart_tx(usart_tx_buf,0);
+	
+	/* Transmit Data Buffer Register (0x09)	*/
+	memset(spi_tx_buf,0,DW1000_SPI_BUF_SIZE);
+	memset(spi_rx_buf,0,DW1000_SPI_BUF_SIZE);
+	/*	
+	* Copy sended mesage to the application transmit buffer.
+	* All application transmit buffer will be written to TX_BUFFER register using the only SPI transaction.
+	* Algorithm of SPI transaction may be changed in the future.
+	*/ 
+	spi_tx_buf[0]=0xc9;
+	spi_tx_buf[1]=0x80;
+	spi_tx_buf[2]=0x00;
+	strcpy(spi_tx_buf+3, msg);
+	spi_tr_size=DW1000_TFLEN-0x2;
+	stm32_spi_tr(spi_tx_buf, spi_rx_buf, spi_tr_size, 0);
+	/*	check the first written byte	*/
+	memset(spi_tx_buf,0,DW1000_SPI_BUF_SIZE);
+	memset(spi_rx_buf,0,DW1000_SPI_BUF_SIZE);
+	spi_tx_buf[0]=0x49;
+	spi_tx_buf[1]=0x80;
+	spi_tx_buf[2]=0x00;
+	spi_tr_size=0x10;
+	stm32_spi_tr(spi_tx_buf,spi_rx_buf,spi_tr_size,0);
+	sprintf(usart_tx_buf,"Transmit Buffer Register (reg:09:00):\n\t{0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x,0x%x}\n",
+		spi_rx_buf[0],spi_rx_buf[1],spi_rx_buf[2],spi_rx_buf[3],spi_rx_buf[4],spi_rx_buf[5],spi_rx_buf[6],spi_rx_buf[7]);
+	stm32_usart_tx(usart_tx_buf,0);
+	
+	/*	start transmission	*/
+	return;
 }
